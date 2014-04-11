0cfa91c2f657c303d7b8efb9768a0d376b5ef282
Daniele Mondello <mondello@olomedia.it>
2014-04-11 13:08:23 +0200

    Aggiunto script per generare a video una tabellina  semplicemente variando la variabile.

---
+++ sezione/tabellina.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Corso PHP & MYSQL 
+ * Utente: Daniele Mondello
+ * Web Site: danielemondello.it
+ * Email:info@danielemondello.it
+ * Argomento: 
+ * Data: 11/04/14
+ */
+
+    //Stampa a video la tebellina di un qualsiasi numero cambiando il valore della variale
+    $numero_tabellina =5;
+    echo '<b>Tabellina del '.$numero_tabellina .'</b><br>';
+
+    for ($multiplo = 1; $multiplo <= 10; ++$multiplo) {
+        $risultato = $numero_tabellina * $multiplo;
+        echo $numero_tabellina." * $multiplo = $risultato <br/>";
+    }
+
