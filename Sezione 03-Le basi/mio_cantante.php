d9c57da4dc355fbb0e7c8e5ee926d8c3eebb0a98
Daniele Mondello <mondello@olomedia.it>
2014-04-12 12:34:16 +0200

    inserito script cantante

---
+++ Sezione 03-Le basi/mio_cantante.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Corso PHP & MYSQL 
+ * Utente: Daniele Mondello
+ * Web Site: danielemondello.it
+ * Email:info@danielemondello.it
+ * Argomento: 
+ * Data: 12/04/14
+ */
+
+$nome_cantante ='vasco';
+
+echo 'il Mio cantante preferito:<br>';
+echo '<b>'.$nome_cantante .'</b>';
+echo '<img src="'.$nome_cantante.'.jpg" alt="testo" />';
